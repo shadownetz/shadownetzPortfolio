e3826939fb0ff619bb4d5856fceb942755138146
shadownetz <okwuzuchiagozie@gmail.com>
2020-02-24 13:59:48 +0100

    update sample content for initial web hosting

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    context = {
+        'ready': False
+    }
+    return render(request, 'blog/index.html', context)
