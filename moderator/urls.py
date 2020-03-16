1db49905a8b0855be1a33c0efe02f94016c1999e
shadownetz <okwuzuchiagozie@gmail.com>
2020-03-16 22:46:49 +0100

    add social life blog template moderator

--- moderator/urls.py
+++ moderator/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('auth', views.auth, name="authentication"),
     path('auth/login', views.auth_login, name="authenticateLogin"),
     path('auth/logout', views.logout_user, name="logoutUser"),
+
     # Developers Hub Category
     path('categories/developershub', category_developershub_views.view_developers_hub_category, name="category_developershub"),
     path('categories/developershub/new', category_developershub_views.new_developers_hub_content, name="category_developershub_add"),
@@ -20,12 +21,18 @@ urlpatterns = [
          name="category_developershub_add_template"),
     path('categories/developershub/new/template/process', category_developershub_views.process_new_developers_hub_content_from_template,
          name="process_category_developershub_add_template"),
+
     # Social Life Category
     path('categories/social-life', category_socialife_views.view_social_life_category,
          name="category_sociallife"),
     path('categories/social-life/new', category_socialife_views.new_social_life_content, name="category_sociallife_add"),
     path('categories/social-life/new/process', category_socialife_views.process_social_life_content,
          name="process_category_sociallife_add"),
+    path('categories/social-life/new/template', category_socialife_views.new_social_life_content_from_template,
+         name="category_sociallife_add_template"),
+    path('categories/social-life/new/template/process', category_socialife_views.process_social_life_content_from_template,
+         name="process_category_sociallife_add_template"),
+
     # Food Category
     path('categories/food', category_food_views.view_food_category, name="category_food"),
     path('categories/food/new', category_food_views.new_food_content, name="category_food_add"),
