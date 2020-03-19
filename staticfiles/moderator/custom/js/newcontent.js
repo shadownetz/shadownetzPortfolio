1dca9b3efe24abb5c102bcfa35d6f85bcfc1125b
shadownetz <okwuzuchiagozie@gmail.com>
2020-03-19 12:50:54 +0100

    revert to default settings

--- staticfiles/moderator/custom/js/newcontent.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.js-select-stock-image').click(function(event){
-        event.preventDefault();
-        let image_element = $(this).children('img')[0];
-        let image_source = $(image_element).attr('src');
-        $('#id_display_image').val(image_source);
-        $('#stockImageModal').modal('hide');
-    })
-});
