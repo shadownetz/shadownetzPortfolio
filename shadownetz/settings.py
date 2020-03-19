1320ff8fc81d91e2fab223c985a762f97ad774e8
shadownetz <okwuzuchiagozie@gmail.com>
2020-03-19 12:53:40 +0100

    changed to heroku db

--- shadownetz/settings.py
+++ shadownetz/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'shadownetz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shadownetzdb',
-        'USER': 'shadownetz',
-        'PASSWORD': 'judoski911',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'shadownetzdb',
+#         'USER': 'shadownetz',
+#         'PASSWORD': 'judoski911',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
